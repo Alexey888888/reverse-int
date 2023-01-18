eeb28a66f622664681483c440e2470141dff1da8
Alexey888888 <brdkmail@gmail.com>
2023-01-18 14:10:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = String(n);
+  let result = '';
+  let i = 0;
+  while (i < n.length) {
+    result = n[i] + result;
+    i = i + 1;
+  }
+  return result;
 }
